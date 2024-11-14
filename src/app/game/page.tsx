a8481b325df185a31d4a78441052ea38b35bfab5
Andrew Chau <andrewchau333@gmail.com>
2024-11-13 16:37:51 -0800

    use client for all pages (better for teaching)

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,19 +1,33 @@
-import GameService from '@/services/GameService';
+'use client';
 
-export default async function Page() {
-  const allGames = await GameService.getAllGames();
+import Game from '@/dto/Game';
+import { useEffect, useState } from 'react';
 
-  const parsedGames = allGames.map((g) => (
-    <li key={g._id}>
-      GameType: {g.gameType}, id: {g._id}
-    </li>
-  ));
+export default function Page() {
+  const [games, setGames] = useState(null);
+
+  async function getAllGames() {
+    const url = '/api/game';
+    const data = await fetch(url).then((data) => data.json());
+    const gamesList = data.map((game: Game) => (
+      <li key={game._id}>
+        GameType: {game.gameType}, id: {game._id}
+      </li>
+    ));
+    setGames(gamesList);
+  }
+
+  useEffect(() => {
+    getAllGames();
+  }, []);
+
+  if (!games) return <div>Loading...</div>;
 
   return (
     <div>
       <h1>All Games</h1>
       <div>
-        <ul>{parsedGames}</ul>
+        <ul>{games}</ul>
       </div>
     </div>
   );
